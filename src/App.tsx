e178fabf36e51c7a308ded0e0e61f6cd4a20b596
Arsen H <77290710+arsenhh-byte@users.noreply.github.com>
2024-04-11 23:14:17 +0300

    The UI has been created

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ function App() {
   
 
   return (
-  <p>Hello World</p> 
+<div className ="container" >
 
-  
+<div className='video-box'> </div>
+  <div className='button-wrapper'>
+    <button> start </button>
+    <button> stop </button>
+  </div>
+  <div className='text-box'>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, porro! Totam facilis rerum porro necessitatibus, eveniet accusantium recusandae voluptates fuga fugit dolor sit dolore pariatur repellendus ut architecto, delectus debitis?
+
+  </div>
+  <button className='btn'>Upload</button>
+
+</div>
+
   )
 }
 
