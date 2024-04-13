c3df40b8444a6050e34567a13d718298bbdaba66
Arsen H <77290710+arsenhh-byte@users.noreply.github.com>
2024-04-13 18:12:03 +0300

    Bootstrap elements have been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
@@ -8,15 +10,14 @@ function App() {
 
 <div className='video-box'> </div>
   <div className='button-wrapper'>
-    <button> start </button>
-    <button> stop </button>
+    <Button variant="outline-primary">Start </Button>{' '}
+    <Button variant="outline-danger">Stop </Button>{' '}
   </div>
   <div className='text-box'>
 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, porro! Totam facilis rerum porro necessitatibus, eveniet accusantium recusandae voluptates fuga fugit dolor sit dolore pariatur repellendus ut architecto, delectus debitis?
 
   </div>
-  <button className='btn'>Upload</button>
-
+  <Button variant="outline-dark">Upload </Button>{' '}
 </div>
 
   )
